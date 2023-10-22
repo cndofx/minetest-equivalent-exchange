dcf08815b850e8cb103c052cb32f804b640550b6
cndofx <19335833+cndofx@users.noreply.github.com>
2023-10-22 15:55:58 -0600

    add dark and red matter axe

--- eqex/src/tools.lua
+++ eqex/src/tools.lua
@@ -133,3 +133,69 @@ minetest.register_craft({
 })
 
 -- Axe
+
+minetest.register_tool("eqex:axe_dark_matter", {
+    description = "Dark Matter Axe",
+    inventory_image = "eqex_axe_dark_matter.png",
+    wield_image = "eqex_axe_dark_matter.png",
+    tool_capabilities = {
+        full_punch_interval = 0.9,
+        max_drop_level = 1,
+        groupcaps = {
+            choppy = {
+                times = {
+                    [1] = 1.7,
+                    [2] = 0.7,
+                    [3] = 0.4,
+                },
+                uses = 0,
+                maxlevel = 3,
+            }
+        },
+        damage_groups = { fleshy = 9 },
+        groups = { axe = 1 }
+    }
+})
+
+minetest.register_craft({
+    output = "eqex:axe_dark_matter",
+    type = "shaped",
+    recipe = {
+        { "eqex:dark_matter", "eqex:dark_matter" },
+        { "eqex:dark_matter", "default:diamond" },
+        { "",                 "default:diamond" },
+    }
+})
+
+minetest.register_tool("eqex:axe_red_matter", {
+    description = "Red Matter Axe",
+    inventory_image = "eqex_axe_red_matter.png",
+    wield_image = "eqex_axe_red_matter.png",
+    tool_capabilities = {
+        full_punch_interval = 0.9,
+        max_drop_level = 1,
+        groupcaps = {
+            choppy = {
+                times = {
+                    [1] = 1.4,
+                    [2] = 0.5,
+                    [3] = 0.3,
+                },
+                uses = 0,
+                maxlevel = 3,
+            }
+        },
+        damage_groups = { fleshy = 11 },
+        groups = { axe = 1 }
+    }
+})
+
+minetest.register_craft({
+    output = "eqex:axe_red_matter",
+    type = "shaped",
+    recipe = {
+        { "eqex:red_matter", "eqex:red_matter" },
+        { "eqex:red_matter", "eqex:axe_dark_matter" },
+        { "",                "eqex:dark_matter" },
+    }
+})
