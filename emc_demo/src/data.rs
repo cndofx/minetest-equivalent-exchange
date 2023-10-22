65da621913f01c851671f04dbb5b9507647b8efc
cndofx <19335833+cndofx@users.noreply.github.com>
2023-10-21 20:35:42 -0600

    emc demo getting abandoned for now

--- emc_demo/src/data.rs
+++ emc_demo/src/data.rs
@@ -12,8 +12,23 @@ pub static RECIPES: Lazy<HashMap<&str, Vec<Recipe>>> = Lazy::new(|| {
     // only works because the json is minified to one line per object
     for line in RAW_RECIPES.lines() {
         let recipe: Recipe = serde_json::from_str(line).unwrap();
-        map.entry(recipe.output).or_insert(Vec::new()).push(recipe);
+        map.entry(recipe.output.item)
+            .or_insert(Vec::new())
+            .push(recipe);
     }
+    map
+});
+
+pub static DEFAULT_EMC: Lazy<HashMap<&str, i32>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    map.insert("default:cobble", 1);
+    map.insert("default:diamond", 8192);
+    map
+});
+
+pub static CUSTOM_EMC: Lazy<HashMap<&str, i32>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    map.insert("default:axe_steel", 1234);
     map
 });
 
@@ -21,14 +36,42 @@ pub static RECIPES: Lazy<HashMap<&str, Vec<Recipe>>> = Lazy::new(|| {
 pub struct Recipe<'a> {
     pub items: Option<Vec<Option<&'a str>>>,
     pub method: &'a str,
-    pub output: &'a str,
+    // pub output: &'a str,
+    // #[serde(deserialize_with = "output")]
+    // pub output: Output,
+    pub output: Output<'a>,
     #[serde(rename = "type")]
     pub recipe_type: &'a str,
-    #[serde(deserialize_with = "width")]
+    #[serde(skip)]
     pub width: u8,
 }
 
-fn width<'de, D: Deserializer<'de>>(deserializer: D) -> Result<u8, D::Error> {
-    let num: f32 = Deserialize::deserialize(deserializer)?;
-    Ok(num as u8)
+#[derive(Debug, Deserialize)]
+#[serde(from = "OutputStr")]
+pub struct Output<'a> {
+    pub item: &'a str,
+    pub count: u8,
+}
+
+#[derive(Deserialize)]
+struct OutputStr<'a> {
+    s: &'a str,
+}
+
+// fn output<'de: 'static, D: Deserializer<'de>>(deserializer: D) -> Result<Output, D::Error> {
+//     let s: &str = Deserialize::deserialize(deserializer)?;
+//     let mut split = s.split(' ');
+//     let item = split.next().unwrap();
+//     let count = split.next().unwrap_or("1").parse::<u8>().unwrap();
+
+//     Ok(Output { item, count })
+// }
+
+impl<'a> From<OutputStr<'a>> for Output<'a> {
+    fn from(value: OutputStr<'a>) -> Self {
+        let mut split = value.s.split(' ');
+        let item = split.next().unwrap();
+        let count = split.next().unwrap_or("1").parse::<u8>().unwrap();
+        Output { item, count }
+    }
 }
--- emc_demo/src/main.rs
+++ emc_demo/src/main.rs
@@ -1,12 +1,103 @@
+use std::{collections::HashMap, default};
+
 use data::{ITEMS, RECIPES};
+
+use crate::data::{CUSTOM_EMC, DEFAULT_EMC};
 
 mod data;
 
+type Cache = HashMap<&'static str, i32>;
+
 fn main() {
-    for item in ITEMS.iter() {
-        println!("{item}");
+    let mut cache: Cache = HashMap::new();
+
+    // dbg!(get_emc_for("default:diamond", &mut cache));
+    // dbg!(get_emc_for("default:diamondblock", &mut cache));
+    // dbg!(get_emc_for("default:furnace", &mut cache));
+    // dbg!(get_emc_for("default:axe_diamond", &mut cache));
+
+    for recipe in RECIPES.iter() {
+        println!("{recipe:#?}");
     }
-    for recipes in RECIPES.iter() {
-        dbg!(&recipes);
+}
+
+fn get_emc_for(itemstring: &'static str, mut cache: &mut Cache) -> i32 {
+    println!("\n\ngetting emc for '{itemstring}'");
+    // make sure this item exists
+
+    // hack to make dealing with groups easier
+    match itemstring {
+        "group:stone" => return get_emc_for("default:stone", &mut cache),
+        "group:stick" => return get_emc_for("default:stick", &mut cache),
+        _ => {}
     }
+
+    if !ITEMS.contains(&itemstring) {
+        println!("item doesnt exist");
+        return -1;
+    }
+
+    // check if this item has a cached emc value
+    if let Some(value) = cache.get(itemstring) {
+        println!("emc of {value} found in cache");
+        return *value;
+    }
+
+    // check if this item has a custom emc value
+    if let Some(value) = CUSTOM_EMC.get(itemstring) {
+        println!("emc of {value} found in CUSTOM_EMC");
+        cache.insert(itemstring, *value);
+        return *value;
+    }
+
+    // check if this item has a default emc value
+    if let Some(value) = DEFAULT_EMC.get(itemstring) {
+        println!("emc of {value} found in DEFAULT_EMC");
+        cache.insert(itemstring, *value);
+        return *value;
+    }
+
+    // no emc found, calculate from crafting components
+    println!("no emc found, calculating...");
+    let mut lowest_total_emc = -1;
+    let Some(recipes) = RECIPES.get(itemstring) else {
+        cache.insert(itemstring, -1);
+        return -1;
+    };
+    // for every recipe that creates this item
+    for recipe in recipes {
+        let mut total_emc = 0;
+        let mut all_ingredients_have_emc = true;
+        dbg!(&recipe);
+        if let Some(ingredients) = &recipe.items {
+            for ingredient in ingredients {
+                if let Some(ingredient) = ingredient {
+                    let emc = get_emc_for(ingredient, &mut cache);
+                    if emc > 0 {
+                        total_emc += emc;
+                    } else {
+                        all_ingredients_have_emc = false;
+                    }
+                }
+            }
+        }
+        total_emc = total_emc / recipe.output.count as i32;
+        if all_ingredients_have_emc
+            && total_emc > 0
+            && (total_emc < lowest_total_emc || lowest_total_emc == -1)
+        {
+            lowest_total_emc = total_emc;
+        }
+    }
+    // for recipes in RECIPES.get(itemstring) {
+    //     let mut total_emc = 0;
+    //     let mut all_ingredients_have_emc = true;
+    //     dbg!(recipe);
+    //     for ingredient in recipe.
+    // }
+
+    // todo!()
+    println!("calculated emc value of {lowest_total_emc} for {itemstring}");
+    cache.insert(itemstring, lowest_total_emc);
+    lowest_total_emc
 }
